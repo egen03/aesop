94fb23a6804c14e50477d969b6b7036649500e3a
Student_PSXGA136 <deablosf@hotmail.com>
2020-07-18 22:50:39 -0400

    Half way there

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,40 +4,56 @@ import './Styles/footer.css';
 class Footer extends Component {
     render() {
         return (
-            <div className="footer">
-                <div className="emailslot"> <p>I would like to receive communications about Aesop  productions, services, stores and events and matters <br />of cultural interest.</p> 
-                    <input type="text" size="50" placeholder="Email address"/>
+            <div>
+                <div className="quote">
+                    
+                        <p>'And your very flesh shall be a great poem'</p>
+                        <p className="folink">Wait Whitman</p>
+                    
+                    
                 </div>
-                <div className="cluster">
-                <ul className="footerlinks" aria-label="Contact Us">
-                    <li>Contact Us</li>
-                    <li>FAQs</li>
-                    <li>Delivery and Returns</li>
-                    <li>Track your order</li>
-                    <li>Check gift card Balance</li>
-                    <li>Terms and conditions</li>
-                    <li>Accessibly</li>
-                </ul>
 
-                <ul className="footerlinks" aria-label="About">
-                    <li>Our Story</li>
-                    <li>Careers</li>
-                    <li>Press</li>
-                    <li>Corporate gifts</li>
-                    <li>Facial appointments</li>
-                </ul>
+                <div className="borderline"></div>
+                <div className="footer">
+                    <div className="emailslot"> 
+                        <p>I would like to receive communications about Aesop  productions, services, stores and events and matters <br />of cultural interest.
+                        </p> 
+                        <input type="text" size="50" placeholder="Email address"/>
+                    </div>
+                    <div className="cluster">
+                        <div className="footerlinks">
+                            <p> <span>Contact Us</span></p>
+                            <p className="folink">Contact Us
+                            <br />FAQs
+                            <br />Delivery and Returns
+                            <br />Track your order
+                            <br />Check gift card Balance
+                            <br />Terms and conditions
+                            <br />Accessibly</p>
+                        </div>
+                    
+                        <div className="footerlinks">
+                            <p> <span>About</span> </p>
+                            <p className="folink">Our Story <br />Careers 
+                            <br />Press 
+                            <br />Corporate gifts 
+                            <br />Facial appointments</p>
+                            
+                        </div>
+                    
+                        <div className="footerlinks">
+                            <p> <span>Social</span> </p>
+                            <p className="folink">Instagram <br />Twitter <br />Linkedin</p>
+                        </div>
 
-                <ul className="footerlinks" aria-label="Social">
-                    <li>Instagram</li>
-                    <li>Twitter</li>
-                    <li>Linkedin</li>
-                </ul>
 
+                    </div>
+                    
                 </div>
                 <div className="blackBar">
-                    <div>&copy;	Aesop</div>
-                    <div className="usa">United States</div> 
-                </div>
+                        <div>&copy;	Aesop</div>
+                        <div className="usa">United States</div> 
+                    </div>
             </div>
         )
     }
