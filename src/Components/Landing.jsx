5a1d614a35e79e327122ea18db07ccaea0b5605d
honeyai <contact.jessicasanchez@gmail.com>
2020-07-20 17:54:45 -0400

    finalized landing image

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import './Styles/landing.css'
-import '../assets/images/Aesop-Homepage-Banner.jpg'
-import Title from './Title'
+import './Styles/landing.css';
+import '../assets/images/Aesop-Homepage-Banner.jpg';
+import Title from './Title.jsx';
+import Subtitle from './Subtitle.jsx';
+import Logo from './Logo';
 
 const Landing = () => {
   return (
     <div className="landingContainer">
       <div className="titleContainer">
-        <Title className="title" title="Formulation for skin, hair, home and body"/>
+        <Logo/>
+        <div className="wordsContainer">
+          <Title title="Formulations for skin, hair, home and body"/>
+          <Subtitle subtitle="Created with sensory pleasure in mind"/>
+        </div>
       </div>
       <span className="landing">
       </span>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Login from './Login';
 import './Styles/navBar.css';
 
