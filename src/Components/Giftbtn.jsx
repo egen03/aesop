a76adc0dfa5c57e7c48dc7634c3f5c3cc6f0447f
Student_GA111 <egen03@gmail.com>
2020-07-22 09:58:46 -0400

    work

---
+++ src/Components/Giftbtn.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Styles/giftbtn.css';
+
+
+
+const Giftbtn = () => {
+  return (
+    <div>
+      <button className="giftbtn">Explore seasonal Gift Kits -></button> 
+      <div></div>    
+    </div>
+  );
+};
+
+export default Giftbtn;
