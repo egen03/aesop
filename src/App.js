e16635d82968078e1c64bd91d33ab15d1e8edfdf
Student_PSXGA136 <deablosf@hotmail.com>
2020-07-21 22:32:28 -0400

    jeez

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+
+      <Footer />
     </div>
   );
 }
